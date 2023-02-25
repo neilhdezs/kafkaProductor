3fea38f6af1dc9922ddd0569b5adc8fb0f697621
neilhdezs <hernandezsalvadorneil@gmail.com>
2023-02-25 14:07:08 +0100

    Initial commit

---
+++ src/main/java/es/nhs/kafkaproductor/KafkaProductorApplication.java
@@ -0,0 +1,13 @@
+package es.nhs.kafkaproductor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/nhs/kafkaproductor/KafkaProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package es.nhs.kafkaproductor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
