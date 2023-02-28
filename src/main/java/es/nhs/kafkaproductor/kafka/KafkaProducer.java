be860fe8d2eec4acfbfae41a528ca2cd13f4e0c9
neilhdezs <hernandezsalvadorneil@gmail.com>
2023-02-28 23:17:53 +0100

    change to autowired

--- src/main/java/es/nhs/kafkaproductor/kafka/KafkaProducer.java
+++ src/main/java/es/nhs/kafkaproductor/kafka/KafkaProducer.java
@@ -2,6 +2,7 @@ package es.nhs.kafkaproductor.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,8 @@ public class KafkaProducer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducer.class);
 
+    @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
-
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     public void sendMessage(String message){
         LOGGER.info(String.format("Message sent %s", message));
